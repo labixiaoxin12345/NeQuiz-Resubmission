97976e63fcda4c782a3870f972f108fbafbd418c
labixiaoxin12345 <vivianleeching@hotmail.com>
2022-12-29 00:06:11 +0800

    updated js code

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+//a c b c a d
+let questions = [
+    {
+    numb: 1,
+    question: "Who was the Minister for Finance in Singapore's first cabinet?",
+    answer: "a. Goh Keng Swee",
+    options: [
+      'a. Goh Keng Swee',     //correct answer
+      'b. Ong Pang Boon',
+      'c. Toh Chin Chye',
+      'd. Sinnathamby Rajaratnam'
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who was the Minister for Home Affairs in Singapore's first cabinet?",
+    answer: "c. Ong Pang Boon",
+    options: [
+      'a. Ahmad Ibrahim',
+      'b. Goh Keng Swee',
+      'c. Ong Pang Boon',          //correct answer
+      'd. Toh Chin Chye'
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the Minister for Culture in Singapore's first cabinet?",
+    answer: "b. Sinnathamby Rajaratnam",
+    options: [
+      'a. Ahmad Ibrahim',
+      'b. Sinnathamby Rajaratnam',      //correct answer
+      'c. Toh Chin Chye',
+      'd. Yusof Ishak'
+
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who was the Deputy Prime Minister in Singapore's first cabinet?",
+    answer: "c. Toh Chin Chye",
+    options: [
+      'a. Sinnathamby Rajaratnam',
+      'b. Goh Keng Swee',      
+      'c. Toh Chin Chye',        //correct answer
+      'd. Ong Pang Boon'
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who was the Minister for Health in Singapore's first cabinet?",
+    answer: "a. Ahmad Ibrahim",
+    options: [
+      'a. Ahmad Ibrahim',       //correct answer
+      'b. Toh Chin Chye',      
+      'c. Goh Keng Swee',
+      'd. Tony Tan Keng Yam'
+    ]
+  },
+    {
+    numb: 6,
+    question: "Who was the Prime Minister in Singapore's first cabinet?",
+    answer: "d. Lee Kuan Yew",
+    options: [
+      "a. Yusof Ishak",
+      "b. Tony Tan Keng Yam",
+      "c. Goh Keng Swee",
+      "d. Lee Kuan Yew"       //correct answer
+    ]
+  },
+];
