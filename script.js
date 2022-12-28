e2615d96485e7cdc52184f73164cdb58a58f4d9d
labixiaoxin12345 <vivianleeching@hotmail.com>
2022-12-29 00:13:43 +0800

    updated display

--- script.js
+++ script.js
@@ -138,15 +138,15 @@ function showResult(){
     if (userScore > 3){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+        scoreText.innerHTML = scoreTag + `questions correct`;  //adding new span tag inside score_Text
     }
     else if(userScore > 1){ // if user scored more than 1
         let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
+        scoreText.innerHTML = scoreTag + `questions correct`;
     }
     else{ // if user scored less than 1
         let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
+        scoreText.innerHTML = scoreTag + `questions correct`;
     }
 }
 function startTimer(time){
